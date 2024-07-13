041f263276a83170bc44e8dc65dad4f6aeee599e
Alex <alexbrunoalexandrino@gmail.com>
2024-07-13 19:22:36 -0400

    fix: get activities

--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { dayjs } from '../lib/day';
-import { z } from "zod";
+import { date, z } from "zod";
 import { prisma } from "../lib/prisma";
 
 export async function getActivities(app: FastifyInstance) {
@@ -16,14 +16,32 @@ export async function getActivities(app: FastifyInstance) {
 
         const trip = await prisma.trip.findUnique({
             where: {id: tripId},
-            include: { activies: true}
+            include: { activies: {
+                orderBy: {
+                    accours_at: 'asc'
+                }
+            }}
         })
 
         if (!trip){
             throw new Error('Trip not found')
         }
 
+        const differenceInDaysBetweenTripStartAndEnd = dayjs(trip.ends_at).diff(trip.starts_at, 'days')
 
-        return { activities: trip.activies }
+        const activities = Array.from({ length: differenceInDaysBetweenTripStartAndEnd + 1}).map((_, index) => {
+            const date = dayjs(trip.starts_at).add(index, 'days')
+
+            return {
+                date: date.toDate(),
+                activities: trip.activies.filter(activities => {
+                    return dayjs(activities.accours_at).isSame(date, 'day')
+                })
+            }
+        })
+
+
+
+        return { activities }
     })
 }
